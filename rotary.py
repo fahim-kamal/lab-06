1aed887476fe969f0085dc147423e21b1c1b5ff5
fkamal <fkamal@usc.edu>
2023-03-03 11:52:30 -0800

    edits import syntaxx

--- rotary.py
+++ rotary.py
@@ -8,11 +8,11 @@ class RotarySensor():
         self.GROVE_VCC = 5
         self.FULL_ANGLE = 300
     
-        grovepi.pinMode(self.port, "INPUT")
+        pinMode(self.port, "INPUT")
         time.sleep(1)
 
     def read(self):
-        sensor_value = grovepi.analogRead(self.port)
+        sensor_value = analogRead(self.port)
         voltage = sensor_value * self.ADC_REF / 1023
         degrees = voltage * self.FULL_ANGLE / self.GROVE_VCC
         return round(degrees)
