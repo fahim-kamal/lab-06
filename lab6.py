6399ead220bc3ab1df6b4446a6d6f37cb39d86fd
fkamal <fkamal@usc.edu>
2023-03-03 11:45:55 -0800

    initial test

--- lab6.py
+++ lab6.py
@@ -1,6 +1,42 @@
 from grove_rgb_lcd import *
+from grovepi import *
+from rotary import RotarySensor
+
+class data:
+    def __init__(self, threshold, distance):
+        self.threshold = threshold
+        self.distance = distance
+
+
+def getData(US_PIN, sensor: RotarySensor):
+    # Get reading from rotary angle detector and ultrasonic sensor
+    angle = sensor.read()
+    distance_cm = ultrasonicRead(US_PIN)
+
+    # Convert data to suitable format
+    threshold_cm = round(517 / angle)
+
+    result = data(threshold_cm, distance_cm)
+
+    return result
+
+def display(result: data):
+    line1 = " {}cm ".format(result.threshold)
+    line2 = " {}cm".format(result.distance)
+
+    line1.ljust(" ", 16)
+    line2.ljust(" ", 16)
+
+    setText(line1 + line2)
+
 
 if __name__ == "__main__":
-    setrgb(0, 0, 0)
+    rs = RotarySensor(1)
+    res = getData(2, rs)
+
+    # Green: rgb(124, 252, 0)
+    setRGB(124, 242, 0)
+    
+
 
     
--- rotary.py
+++ rotary.py
@@ -1,20 +1,19 @@
 import time
-import grovepi
+from grovepi import *
 
-ADC_REF = 5
-GROVE_VCC = 5
-FULL_ANGLE = 300
+class RotarySensor():
+    def __init__(self, port):
+        self.port = port
+        self.ADC_REF = 5
+        self.GROVE_VCC = 5
+        self.FULL_ANGLE = 300
+    
+        grovepi.pinMode(self.port, "INPUT")
+        time.sleep(1)
 
-def initRotarySensor(port):
-    grovepi.pinMode(port, "INPUT")
-    time.sleep(1)
-
-def getDegrees(port):
-    sensor_value = grovepi.analogRead(port)
-
-    voltage = sensor_value * ADC_REF / 1023; 
-
-    degrees = voltage * FULL_ANGLE / GROVE_VCC
-
-    return degrees
+    def read(self):
+        sensor_value = grovepi.analogRead(self.port)
+        voltage = sensor_value * self.ADC_REF / 1023
+        degrees = voltage * self.FULL_ANGLE / self.GROVE_VCC
+        return round(degrees)
 
